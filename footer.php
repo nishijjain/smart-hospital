33c430564c728f5764280b05fcef992975b67b47
Nishi Jain <nishijjain28@gmail.com>
2019-11-05 19:14:40 +0530

    Uploading files

---
+++ footer.php
@@ -0,0 +1,33 @@
+
+<link href="css/menu.css" rel="stylesheet" type="text/css" />
+<br>
+<div class="container-fluid footer">
+    <div class="row">
+    <br>
+        <div class="col-md-10 col-md-push-2">
+        <div style="text-align: left;" class="col-md-2"><h5 style="font-size: 17px;" >Patient Info</h5>
+
+        <p><a style="color: #767773" href="patin.php">Patient Registration</a><br>
+        <a style="color: #767773" href="patiinfo.php">Patient Information</a><br>
+        <a style="color: #767773" href="petsearch.php">Patient Search</a><br>
+        </p>
+
+        </div>
+        <div style="text-align: left;" class="col-md-2"><h5 style="font-size: 17px;" >Staff Info</h5>
+        <p><a style="color: #767773" href="staff.php">Add Staff Members</a><br>
+        <a style="color: #767773" href="staffmemd.php">Staff Members Details</a><br>
+        <a style="color: #767773" href="sestaffactmem.php">Search Staff Members</a><br>
+        <a style="color: #767773" href="staffactmem.php">Active Members</a><br>
+        </p>
+        </div>
+        <div style="text-align: left;" class="col-md-2"><h5 style="font-size: 17px;" >Patient Invoices</h5>
+        <p><a style="color: #767773" href="invoinfo.php">OPD Patient Invoices</a><br>
+        <a style="color: #767773" href="invoadinfo.php">Admitted Patient Invoices</a></p></div>
+        <div style="text-align: left;" class="col-md-2"><h5 style="font-size: 17px;" >Room Information</h5>
+        <p><a style="color: #767773" href="roomavi.php">Room Availability</a></p></div>
+        <div style="text-align: left;" class="col-md-2"><h5 style="font-size: 17px;" >Admitted Patient Information</h5>
+        <p id=""><a style="color: #767773" href="admite.php">Admitted Patient Information</a></p></div>
+        </div>
+        </div>
+    </div>
+   
